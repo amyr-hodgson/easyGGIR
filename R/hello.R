b05ac21cae6efea50de00dd63d2c8eaff3c92263
Amy_Hodgson <amyhodgson777@gmail.com>
2019-07-17 13:11:59 +0100

    more setup

--- R/hello.R
+++ R/hello.R
@@ -1,8 +1,4 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
+
 # You can learn more about package authoring with RStudio at:
 #
 #   http://r-pkgs.had.co.nz/
